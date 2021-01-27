e4ffbebea85233240a97fc92251140d9101ff425
Fenyi <fenyi.shi@seeed.cc>
2021-01-27 13:47:53 +0800

    update

--- main.ts
+++ main.ts
@@ -695,9 +695,11 @@ namespace grove {
      * Check if a specified gesture is detected and return as "Ture" or "False"
      * @param g type of g to detect
      */
-    // % blockId=ggesture block="%key"
+  
 
     //% blockId=grove_gesture_create_event block="on Gesture|%gesture"
+    //% g.fieldEditor="gridpicker" g.fieldOptions.columns=4
+    //% g.fieldOptions.tooltips="false" g.fieldOptions.width="250"
     //% group="Sensor"
     export function ggesture(g: GroveGesture): boolean {
         if (g==1 || g==2 || g==3 || g==4 || g==5 || g==6 || g==7 || g==8 || g==9  )
